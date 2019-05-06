70efde7ddce9570fdb9a95689f022f76de0409c4
robbbowes <up666724@myport.ac.uk>
2019-05-06 20:30:41 +0200

    AnswerObjectClass unit tests, displayAuestions and checkAnswer functionality

--- app/src/main/java/com/example/braintrainerapp/MainActivity.java
+++ app/src/main/java/com/example/braintrainerapp/MainActivity.java
@@ -13,22 +13,36 @@ import static java.util.Arrays.asList;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private int correctAnswerIndex;
+    private int correctAnswers;
+    private int totalAnswered;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        createAndDisplayQuestion();
-//        int number1 = generateRandomNumber(50) + 1;
-//        int number2 = generateRandomNumber(50) + 1;
-//        int operator = generateRandomNumber(2);
-//
-//        AnswerObjectClass answerObject = generateRandomAnswer(number1, number2, operator);
-//
-//        TextView questionTextView = findViewById(R.id.question);
-//        questionTextView.setText(answerObject.getAnswerString());
+        createAndDisplayQuestions();
+
+    }
+
+    public void checkAnswer(View view) {
+        String feedbackString;
+        totalAnswered++;
+        int tag = Integer.parseInt(view.getTag().toString());
+        if (tag == correctAnswerIndex) {
+            correctAnswers++;
+            feedbackString = "Correct!";
+        } else {
+            feedbackString = "Wrong!";
+        }
+        TextView feedbackTextView = findViewById(R.id.feedbackTextView);
+        feedbackTextView.setText(feedbackString);
+
+        TextView scoreTextView = findViewById(R.id.score);
+        String scoreString = correctAnswers + "/" + totalAnswered;
+        scoreTextView.setText(scoreString);
+        createAndDisplayQuestions();
     }
 
     //returns random number from 0 to max
@@ -51,34 +65,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void reload(View view) {
-        createAndDisplayQuestion();
+        createAndDisplayQuestions();
     }
 
-    public void createAndDisplayQuestion() {
+    public void createAndDisplayQuestions() {
         int number1 = generateRandomNumber(50) + 1;
         int number2 = generateRandomNumber(50) + 1;
         int operator = generateRandomNumber(2);
 
         AnswerObjectClass answerObject = generateRandomAnswer(number1, number2, operator);
-
         TextView questionTextView = findViewById(R.id.question);
         questionTextView.setText(answerObject.getAnswerString());
 
+        displayQuestions(answerObject);
+    }
+
+    // Renders all questions and sets the class variable as the index of the correct one
+    private void displayQuestions(AnswerObjectClass correctAnswer) {
         List<TextView> answerTextViews = new ArrayList<>(asList(
                 (TextView) findViewById(R.id.answer1),
                 (TextView) findViewById(R.id.answer2),
                 (TextView) findViewById(R.id.answer3),
                 (TextView) findViewById(R.id.answer4)
         ));
-        int randomTextViewInt = generateRandomNumber(4);
-        TextView correctAnswerTextView = answerTextViews.get(randomTextViewInt);
+        correctAnswerIndex = generateRandomNumber(answerTextViews.size());
+        TextView correctAnswerTextView = answerTextViews.get(correctAnswerIndex);
         answerTextViews.remove(correctAnswerTextView);
-        correctAnswerTextView.setText(String.valueOf(answerObject.getAnswerInt()));
+        correctAnswerTextView.setText(String.valueOf(correctAnswer.getAnswerInt()));
         for (TextView answerTextView : answerTextViews) {
             answerTextView.setText(String.valueOf(generateRandomNumber(50) + 1));
         }
-
-        
     }
 
 }
--- app/src/test/java/com/example/braintrainerapp/ExampleUnitTest.java
+++ app/src/test/java/com/example/braintrainerapp/ExampleUnitTest.java
@@ -9,9 +9,38 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class ExampleUnitTest extends MainActivity{
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void answerInt_additionIsCorrect() {
+        int operatorIndex = 0; // +
+        AnswerObjectClass answerObject = generateRandomAnswer(17, 32, operatorIndex);
+        assertEquals(49, answerObject.getAnswerInt());
+    }
+
+    @Test
+    public void answerInt_subtractionIsCorrect() {
+        int operatorIndex = 1; // -
+        AnswerObjectClass answerObject = generateRandomAnswer(27, 50, operatorIndex);
+        assertEquals(-23, answerObject.getAnswerInt());
+    }
+
+    @Test
+    public void answerString_additionIsCorrect() {
+        int operatorIndex = 0; // +
+        AnswerObjectClass answerObject = generateRandomAnswer(13, 38, operatorIndex);
+        assertEquals("13 + 38 ?", answerObject.getAnswerString());
+    }
+
+    @Test
+    public void answerString_subtractionIsCorrect() {
+        int operatorIndex = 1; // 0
+
+        AnswerObjectClass answerObject = generateRandomAnswer(43, 20, operatorIndex);
+        assertEquals("43 - 20 ?", answerObject.getAnswerString());
     }
 }
